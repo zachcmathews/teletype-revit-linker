004f8e3eb83e548e35be49e8f3e930db3bfb9f9c
Zachary Mathews <zachary.c.mathews@gmail.com>
2020-05-23 16:13:18 -0400

    wrote some tests

--- spec/teletype-revit-linker-package-spec.js
+++ spec/teletype-revit-linker-package-spec.js
@@ -1,9 +1,7 @@
 /* eslint no-undef: off */
 const url = require('url');
-const matchers = require('./matchers');
 const TeletypeRevitLinkerPackage
   = require('../lib/teletype-revit-linker-package');
-
 
 describe('teletype-revit-linker-package', () => {
   atom.config.settings = atom.config.defaultSettings;
@@ -38,20 +36,125 @@ describe('teletype-revit-linker-package', () => {
     });
   });
 
+  describe('deactivate', () => {
+    beforeEach(() => {
+      makeNewPackage();
+      waitsForPromise(() => teletypeRevitLinkerPackage.activate());
+    });
+
+    it('disposes of subscriptions', () => {
+      teletypeRevitLinkerPackage.deactivate();
+      expect(
+        teletypeRevitLinkerPackage
+          .subscriptions
+          .disposables
+      ).toBeNull();
+    });
+
+    it('calls unlockFiles', () => {
+      const unlockFilesSpy = spyOn(teletypeRevitLinkerPackage, 'unlockFiles');
+      teletypeRevitLinkerPackage.deactivate();
+      expect(unlockFilesSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('consumeTeletype', () => {
+    beforeEach(() => {
+      makeNewPackage();
+      waitsForPromise(() => teletypeRevitLinkerPackage.activate());
+    });
+
+    it('provides access to teletype package', () => {
+      teletypeRevitLinkerPackage.consumeTeletype({teletypePackage: 'mock'});
+      expect(teletypeRevitLinkerPackage.teletype).toBeDefined();
+    });
+
+    afterEach(() => {
+      teletypeRevitLinkerPackage.deactivate();
+    });
+  });
+
+  describe('provideTeletypeRevitLinker', () => {
+    beforeEach(() => {
+      makeNewPackage();
+      waitsForPromise(() => teletypeRevitLinkerPackage.activate());
+    });
+
+    it('provides current teletype-revit-linker package', () => {
+      service = teletypeRevitLinkerPackage.provideTeletypeRevitLinker();
+      expect(service).toBe(teletypeRevitLinkerPackage);
+    });
+
+    afterEach(() => {
+      teletypeRevitLinkerPackage.deactivate();
+    });
+  });
+
+  describe('getTeletype', () => {
+    beforeEach(() => {
+      makeNewPackage();
+      waitsForPromise(() => teletypeRevitLinkerPackage.activate());
+    });
+
+    it('returns teletype if consumed', () => {
+      teletypeRevitLinkerPackage.consumeTeletype({teletypePackage: 'mock'});
+      teletypeRevitLinkerPackage.getTeletype().then((teletype) => {
+        expect(teletype).toBe('mock');
+      });
+    });
+
+    it('emits teletype-not-installed appropriately', () => {
+      const emitSpy = spyOn(teletypeRevitLinkerPackage.emitter, 'emit');
+      teletypeRevitLinkerPackage.getTeletype().then((teletype) => {
+        expect(teletype).toBeNull();
+        expect(emitSpy).toHaveBeenCalledWith('teletype-not-installed');
+      });
+    });
+
+    it('emits teletype-disabled appropriately', () => {
+      const emitSpy = spyOn(teletypeRevitLinkerPackage.emitter, 'emit');
+      atom.packages.loadPackage('teletype');
+      teletypeRevitLinkerPackage.getTeletype().then((teletype) => {
+        expect(teletype).toBeNull();
+        expect(emitSpy).toHaveBeenCalledWith('teletype-disabled');
+      });
+    });
+
+    it('emits teletype-not-activated appropriately', () => {
+      const emitSpy = spyOn(teletypeRevitLinkerPackage.emitter, 'emit');
+      teletypeRevitLinkerPackage.getTeletype().then((teletype) => {
+        expect(teletype).toBeNull();
+        expect(emitSpy).toHaveBeenCalledWith('teletype-not-activated');
+      });
+    });
+
+    afterEach(() => {
+      teletypeRevitLinkerPackage.deactivate();
+    });
+  });
+
   describe('handleURI', () => {
-    const newUri = 'atom://teletype-revit-linker/new?file=file.txt';
-    const joinUri = 'atom://teletype-revit-linker/join?teletypeURI=uri';
+    beforeEach(() => {
+      makeNewPackage();
+      waitsForPromise(() => teletypeRevitLinkerPackage.activate());
+    });
 
     it('calls sharePortal for path /new', () => {
+      const newUri = 'atom://teletype-revit-linker/new?file=file.txt';
       const sharePortalSpy = spyOn(teletypeRevitLinkerPackage, 'sharePortal');
       teletypeRevitLinkerPackage.handleURI(url.parse(newUri));
       expect(sharePortalSpy).toHaveBeenCalled();
     });
 
     it('calls joinPortal for path /join', () => {
+      const joinUri = 'atom://teletype-revit-linker/join?teletypeURI=uri';
       const joinPortalSpy = spyOn(teletypeRevitLinkerPackage, 'joinPortal');
       teletypeRevitLinkerPackage.handleURI(url.parse(joinUri));
       expect(joinPortalSpy).toHaveBeenCalled();
+    });
+
+    afterEach(() => {
+      teletypeRevitLinkerPackage.deactivate();
     });
   });
 });
